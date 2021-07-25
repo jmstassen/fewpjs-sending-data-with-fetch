4249e95cbd99c89ac150d90197fdd2d13a251b63
John M. Stassen <john.stassen@gmail.com>
2021-07-25 10:53:07 -0700

    commit tree ca0598b272e7c961c8a51afa14f8b761028a9845

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        const body = document.querySelector("body");
+        const showId = document.createElement("h2");
+        showId.innerHTML = object.id;
+        body.appendChild(showId)
+    })
+    .catch(function(error) {
+        const body = document.querySelector("body");
+        const showMessage = document.createElement("h2");
+        showMessage.innerHTML = error.message;
+        body.appendChild(showMessage)
+    })
+}
